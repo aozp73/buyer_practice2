a2704602da5726534ac21a463a4cdbd897c1e005
aozp73 <aozp73@naver.com>
2023-01-22 10:33:17 +0900

    table, Entity 생성 완료

---
+++ src/main/java/shop/mtcoding/buyer4/model/Purchase.java
@@ -0,0 +1,16 @@
+package shop.mtcoding.buyer4.model;
+
+import java.sql.Timestamp;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Purchase {
+    private int id;
+    private int userId;
+    private int productId;
+    private int count;
+    private Timestamp createdAt;
+}
