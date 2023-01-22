0ac0a5e8421613f11f9ca044416e3a008e09456c
aozp73 <aozp73@naver.com>
2023-01-22 10:04:55 +0900

    프로젝트 세팅 완료

---
+++ src/main/java/shop/mtcoding/buyer4/Buyer4Application.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.buyer4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Buyer4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Buyer4Application.class, args);
+	}
+
+}
---
+++ src/test/java/shop/mtcoding/buyer4/Buyer4ApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.buyer4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Buyer4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
